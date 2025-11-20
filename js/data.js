68797f866516be7885b05e207495cb2649e91483
ertugrulerden <ertugrulerden05@gmail.com>
2025-11-20 12:10:46 +0300

    yga

--- js/data.js
+++ js/data.js
@@ -4,5 +4,11 @@ const chapters = {
     2: chapter2,
     3: chapter3,
     4: chapter4,
-    5: chapter5
+    5: chapter5,
+    6: chapter6,  // YGA-1
+    7: chapter7,  // YGA-2
+    8: chapter8,  // YGA-3
+    9: chapter9,  // YGA-5
+    10: chapter10, // YGA-6
+    11: chapter11 // YGA-Full
 };
